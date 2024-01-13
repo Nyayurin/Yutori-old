aa6e28405e1b2c0b83a08ab10ebdf9fe32859a45
Nyayurn <ChenDSakura@163.com>
2024-01-13 23:53:51 +0800

    抛弃 Maven Central

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,10 @@
 plugins {
     kotlin("jvm") version "1.9.21"
-    `java-library`
     `maven-publish`
-    signing
 }
 
 group = "io.github.nyayurn"
-version = "0.3.0-snapshot"
+version = "0.3.0-SNAPSHOT"
 description = "基于 Satori 协议的 Java 机器人开发工具包"
 java.sourceCompatibility = JavaVersion.VERSION_1_8
 
@@ -32,62 +30,18 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter:$junitVersion")
 }
 
-java {
-    withSourcesJar()
-    withJavadocJar()
-}
-
 tasks.test {
     useJUnitPlatform()
 }
 
-publishing {
-    publications.create<MavenPublication>("maven") {
+publishing.publications {
+    create<MavenPublication>("maven") {
         from(components["java"])
-        pom {
-            name = "Yutori"
-            description = "基于 Satori 协议的 Java 机器人开发工具包"
-            url = "https://github.com/Nyayurn/Yutori"
-
-            licenses {
-                license {
-                    name = "木兰宽松许可证, 第2版"
-                    url = "http://license.coscl.org.cn/MulanPSL2"
-                }
-            }
-
-            developers {
-                developer {
-                    id = "Nyayurn"
-                    name = "Yurn"
-                    email = "ChenDSakura@163.com"
-                }
-            }
-
-            scm {
-                url = "https://github.com/Nyayurn/Yutori"
-                connection = "https://github.com/Nyayurn/Yutori.git"
-            }
-        }
-        repositories {
-            maven {
-                name = "sonatype"
-                url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
-                credentials {
-                    username = findProperty("ossrhUsername") as String?
-                    password = findProperty("ossrhPassword") as String?
-                }
-            }
-        }
     }
 }
 
 kotlin {
     jvmToolchain(8)
-}
-
-signing {
-    sign(publishing.publications)
 }
 
 sourceSets {
